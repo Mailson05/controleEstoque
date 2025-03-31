62ea25162f7472cf4c3f0a8370cde127b9dff8fb
Mailson <mailsonestudo48@gmail.com>
2025-03-31 20:24:36 -0300

    Atualizado

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        String nome;
+        System.out.print("Nome do produto: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Preço do produto: ");
+        double preco = scanner.nextDouble();
+
+        System.out.print("Quantidade inicial em estoque: ");
+        int quantidade = scanner.nextInt();
+
+        Produto produto = new Produto(nome, preco, quantidade);
+
+        System.out.println("\nInformações do produto antes da venda:");
+        produto.exibirInformacoes();
+
+        System.out.print("\nQuantidade vendida: ");
+        int quantidadeVendida = scanner.nextInt();
+        produto.vender(quantidadeVendida);
+
+        System.out.println("\nInformações do produto após a venda:");
+        produto.exibirInformacoes();
+
+        scanner.close();
     }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,3 +1,29 @@
-public class Produto {
+import java.util.Scanner;
 
+class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Produto: " + nome);
+        System.out.printf("Preço: R$ %.2f\n", preco);
+        System.out.println("Quantidade em estoque: " + quantidade);
+    }
+
+    public void vender(int quantidadeVendida) {
+        if (quantidadeVendida > quantidade) {
+            System.out.println("Estoque insuficiente para realizar a venda.");
+        } else {
+            quantidade -= quantidadeVendida;
+            System.out.println("Venda realizada com sucesso!");
+        }
+    }
 }
+
